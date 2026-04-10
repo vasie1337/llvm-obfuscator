bf9e890c5f6e3affd66066b532e462765cc2e1d0
vasie1337 <vasie1337@gmail.com>
2026-04-11 00:22:05 +0200

    build: define export symbol for obfuscator plugin on Windows to ensure proper DLL functionality

--- passes/src/PassPlugin.cpp
+++ passes/src/PassPlugin.cpp
@@ -2,6 +2,16 @@
 
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/Compiler.h"
+
+#if defined(_WIN32)
+// Clang loads this symbol with GetProcAddress; MSVC does not export it unless
+// marked, or the DLL fails with "Plugin entry point not found".
+#define OBFUSCATOR_PLUGIN_ABI __declspec(dllexport)
+#else
+#define OBFUSCATOR_PLUGIN_ABI LLVM_ATTRIBUTE_WEAK
+#endif
+
 using namespace llvm;
 using namespace obfuscator;
 
@@ -111,6 +121,6 @@ llvm::PassPluginLibraryInfo getObfuscatorPluginInfo() {
     };
 }
 
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+extern "C" OBFUSCATOR_PLUGIN_ABI ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
     return getObfuscatorPluginInfo();
 }
